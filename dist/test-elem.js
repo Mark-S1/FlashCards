2797ea290e85a0871d86704cea8131e66eecad4c
Mark <msegedin35work@gmail.com>
2023-10-24 08:29:06 +1300

    Removal of CommonJS reference

--- dist/test-elem.js
+++ dist/test-elem.js
@@ -1,4 +1,3 @@
-"use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,26 +7,24 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MyElement = void 0;
-const lit_1 = require("lit");
-const decorators_js_1 = require("lit/decorators.js");
-let MyElement = class MyElement extends lit_1.LitElement {
+import { LitElement, html } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+let MyElement = class MyElement extends LitElement {
     constructor() {
         super(...arguments);
         this.id = 'STARTING';
     }
     render() {
-        return (0, lit_1.html) `
+        return html `
 		<p>This a test: ${this.id}</p>
 		`;
     }
 };
-exports.MyElement = MyElement;
 __decorate([
-    (0, decorators_js_1.property)(),
+    property(),
     __metadata("design:type", Object)
 ], MyElement.prototype, "id", void 0);
-exports.MyElement = MyElement = __decorate([
-    (0, decorators_js_1.customElement)('test-elem')
+MyElement = __decorate([
+    customElement('test-elem')
 ], MyElement);
+export { MyElement };
